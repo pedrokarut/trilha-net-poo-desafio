2a3b1578f33a3da0a128018cba1a4be36cc1f39d
Pedro Karut Gimenez <pedrokarut@hotmail.com>
2024-09-22 23:06:05 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n = new Nokia("30", "C40", "124578963", 10);
+n.Ligar();
+n.InstalarAplicativo("Whatsapp");
+
+Iphone i = new Iphone("45", "5S", "1245789637889", 20);
+i.Ligar();
+i.InstalarAplicativo("Telegram");
